7a362935e2a20574ace2f845668fa456e44112a7
Juan Manuel <68016784+madpingui@users.noreply.github.com>
2022-04-17 22:13:41 -0500

    Initial commit

---
+++ Assets/CarController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CarController : MonoBehaviour
+{
+    public float fuel = 1;
+    public float fuelConsumption = 0.1f;
+    public Rigidbody2D carRigidbody;
+    public Rigidbody2D backTire;
+    public Rigidbody2D frontTire;
+    public float speed = 20;
+    public float carTorque = 10;
+    private float movement;
+    public Image image;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+        image.fillAmount = fuel;
+    }
+
+    private void FixedUpdate()
+    {
+        if(fuel > 0)
+        {
+            backTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            frontTire.AddTorque(-movement * speed * Time.fixedDeltaTime);
+            carRigidbody.AddTorque(movement * carTorque * Time.fixedDeltaTime);
+        }
+
+        fuel -= fuelConsumption * Mathf.Abs(movement) * Time.fixedDeltaTime;
+    }
+}
